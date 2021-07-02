637a345048042b560607107470088aa223d0f3e6
pierre.c <pierre.c@kakaocorp.com>
2021-07-02 11:20:20 +0900

    project setting

---
+++ src/main/kotlin/house/brandstore/supercoffee/SupercoffeeApplication.kt
@@ -0,0 +1,11 @@
+package house.brandstore.supercoffee
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SupercoffeeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SupercoffeeApplication>(*args)
+}
---
+++ src/test/kotlin/house/brandstore/supercoffee/SupercoffeeApplicationTests.kt
@@ -0,0 +1,13 @@
+package house.brandstore.supercoffee
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SupercoffeeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
